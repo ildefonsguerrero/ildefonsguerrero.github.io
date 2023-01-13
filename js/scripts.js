fc9d8fcd50ce93702833cafd834789b77d8b5786
unknown <serduero@gmail.com>
2023-01-13 19:19:33 +0100

    nuevo site

--- js/scripts.js
+++ js/scripts.js
@@ -94,8 +94,7 @@ $(function () {
     }),
     $(".scrollup").click(function () {
       return $("html, body").animate({ scrollTop: 0 }, 600), !1;
-    }),
-    new Waypoint.Sticky({ element: $(".top")[0] });
+    });
   $("#map_canvas1").addClass("scrolloff"),
     $("#canvas1").on("click", function () {
       $("#map_canvas1").removeClass("scrolloff");
@@ -114,53 +113,6 @@ $(function () {
       $("#map_canvas1").addClass("scrolloff");
     });
 });
-
-/**
- * sticky
- **/
-!(function () {
-  "use strict";
-  function t(s) {
-    (this.options = e.extend({}, i.defaults, t.defaults, s)),
-      (this.element = this.options.element),
-      (this.$element = e(this.element)),
-      this.createWrapper(),
-      this.createWaypoint();
-  }
-  var e = window.jQuery,
-    i = window.Waypoint;
-  (t.prototype.createWaypoint = function () {
-    var t = this.options.handler;
-    this.waypoint = new i(
-      e.extend({}, this.options, {
-        element: this.wrapper,
-        handler: e.proxy(function (e) {
-          var i = this.options.direction.indexOf(e) > -1,
-            s = i ? this.$element.outerHeight(!0) : "";
-          this.$wrapper.height(s),
-            this.$element.toggleClass(this.options.stuckClass, i),
-            t && t.call(this, e);
-        }, this),
-      })
-    );
-  }),
-    (t.prototype.createWrapper = function () {
-      this.$element.wrap(this.options.wrapper),
-        (this.$wrapper = this.$element.parent()),
-        (this.wrapper = this.$wrapper[0]);
-    }),
-    (t.prototype.destroy = function () {
-      this.$element.parent()[0] === this.wrapper &&
-        (this.waypoint.destroy(),
-        this.$element.removeClass(this.options.stuckClass).unwrap());
-    }),
-    (t.defaults = {
-      wrapper: '<div class="sticky-wrapper" />',
-      stuckClass: "stuck",
-      direction: "down right",
-    }),
-    (i.Sticky = t);
-})();
 
 /**
  * elipsis
